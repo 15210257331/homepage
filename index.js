f9a36c89730f80df58a551214a9e38f38dd5e25a
chenxiaofei <15210257331@163.com>
2022-05-24 11:35:20 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const path = require("path")
+const express = require("express")
+const compression = require("compression")
+
+const CONTEXT = `/${process.env.CONTEXT || "angular-ngrx-material-starter"}`
+const PORT = process.env.PORT || 4000
+
+const app = express()
+
+app.use(compression())
+
+app.use("/", express.static(path.resolve(__dirname, "./public")))
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}${CONTEXT}`))
